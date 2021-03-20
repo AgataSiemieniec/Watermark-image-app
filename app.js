5a32df477c9166203d61e03bded46900ba43e7ab
Agata Siemieniec <agata.siemieniec@hotmail.com>
2021-03-20 13:07:35 +0100

    Add try ... catch-er

--- app.js
+++ app.js
@@ -4,29 +4,38 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 
 const addTextWatermarkToImage = async function(inputFile, outputFile, text) {
-  const image = await Jimp.read(inputFile);
-  const font = await Jimp.loadFont(Jimp.FONT_SANS_32_BLACK);
-  const textData = {
-    text: text,
-    alignmentX: Jimp.HORIZONTAL_ALIGN_CENTER,
-    alignmentY: Jimp.VERTICAL_ALIGN_MIDDLE,
-  };
-
-  image.print(font, 0, 0, textData, image.getWidth(), image.getHeight());
-  await image.quality(100).writeAsync(outputFile);
+  try {
+    const image = await Jimp.read(inputFile);
+    const font = await Jimp.loadFont(Jimp.FONT_SANS_32_BLACK);
+    const textData = {
+      text: text,
+      alignmentX: Jimp.HORIZONTAL_ALIGN_CENTER,
+      alignmentY: Jimp.VERTICAL_ALIGN_MIDDLE,
+    };
+    image.print(font, 0, 0, textData, image.getWidth(), image.getHeight());
+    await image.quality(100).writeAsync(outputFile);
+  }
+  catch(error){
+    console.log('Something went wrong (operation error)... Try again!')
+  }
 };
 
 const addImageWatermarkToImage = async function(inputFile, outputFile, watermarkFile) {
-  const image = await Jimp.read(inputFile);
-  const watermark = await Jimp.read(watermarkFile);
-  const x = image.getWidth() / 2 - watermark.getWidth() / 2;
-  const y = image.getHeight() / 2 - watermark.getHeight() / 2;
+  try {
+    const image = await Jimp.read(inputFile);
+    const watermark = await Jimp.read(watermarkFile);
+    const x = image.getWidth() / 2 - watermark.getWidth() / 2;
+    const y = image.getHeight() / 2 - watermark.getHeight() / 2;
 
-  image.composite(watermark, x, y, {
-    mode: Jimp.BLEND_SOURCE_OVER,
-    opacitySource: 0.5,
-  });
-  await image.quality(100).writeAsync(outputFile);
+    image.composite(watermark, x, y, {
+      mode: Jimp.BLEND_SOURCE_OVER,
+      opacitySource: 0.5,
+    });
+    await image.quality(100).writeAsync(outputFile);
+  }
+  catch(error){
+    console.log('Something went wrong (operation error)... Try again!')
+  }
 };
 
 const prepareOutputFilename = (filename) => {
@@ -71,7 +80,8 @@ const startApp = async () => {
       console.log('Success! You made it ;) Chceck your watermarked file');
       startApp();
     } else {
-      console.log('Something went wrong... Try again');
+      console.log('Something went wrong with source file... Try again');
+      startApp();
     }
   }
   else {
@@ -88,7 +98,8 @@ const startApp = async () => {
       console.log('Success! You made it ;) Chceck your watermarked file');
       startApp();
     } else {
-      console.log('Something went wrong... Try again');
+      console.log('Something went wrong with source files... Try again');
+      startApp();
     }
   }
 
